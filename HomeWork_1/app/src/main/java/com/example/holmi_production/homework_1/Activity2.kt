55e8d3595209b2ef11d2066993c82f9f61329ff0
a.holmanov <a.holmanov@gmail.com>
2019-02-15 21:05:27 +0500

    second activity

---
+++ HomeWork_1/app/src/main/java/com/example/holmi_production/homework_1/Activity2.kt
@@ -0,0 +1,13 @@
+package com.example.holmi_production.homework_1
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class Activity2: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity2)
+    }
+
+}

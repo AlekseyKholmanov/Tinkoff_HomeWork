f5a370a5cf03bc7781f225100800f37b698f49e3
AlekseyKholmanov <a.holmanov@gmail.com>
2019-02-22 00:36:19 +0500

    menu option

--- Fragment_Homework/app/src/main/java/com/example/fragment_homework/MainActivity.kt
+++ Fragment_Homework/app/src/main/java/com/example/fragment_homework/MainActivity.kt
@@ -2,11 +2,32 @@ package com.example.fragment_homework
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu,menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.action_add){
+            //TODO
+            Log.d("qwerty","add")
+        }
+        if(item.itemId == R.id.action_delete)
+        {
+            Log.d("qwerty", "delete")
+            //TODO
+        }
+        return super.onOptionsItemSelected(item)
     }
 }

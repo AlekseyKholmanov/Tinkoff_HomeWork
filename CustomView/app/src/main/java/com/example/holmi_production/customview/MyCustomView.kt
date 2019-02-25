5a94c960437d36420c1fdd75a312e52860184c2e
a.holmanov <a.holmanov@gmail.com>
2019-02-25 20:08:31 +0500

    MyCustomView project

---
+++ CustomView/app/src/main/java/com/example/holmi_production/customview/MyCustomView.kt
@@ -0,0 +1,11 @@
+package com.example.holmi_production.customview
+
+import android.content.Context
+import android.view.ViewGroup
+
+class MyCustomView(context:Context?) : ViewGroup(context){
+    override fun onLayout(changed: Boolean, l: Int, t: Int, r: Int, b: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+}

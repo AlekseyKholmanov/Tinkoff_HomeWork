6ac432495619316fee2f12a39c0f13e2278695f5
a.holmanov <a.holmanov@gmail.com>
2019-03-05 09:55:09 +0500

    скелет проекта

--- RecycleView_4/app/src/main/java/com/example/holmi_production/recycleview_4/MainActivity.kt
+++ RecycleView_4/app/src/main/java/com/example/holmi_production/recycleview_4/MainActivity.kt
@@ -2,11 +2,34 @@ package com.example.holmi_production.recycleview_4
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.view.ViewPager
+import android.support.design.widget.TabLayout
+import android.support.v7.widget.Toolbar
+
 
 class MainActivity : AppCompatActivity() {
+    private var toolbar: Toolbar? = null
+    private val tabLayout: TabLayout? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val viewPager:ViewPager = findViewById(R.id.pager)
+        setupViewPager(viewPager)
+
+        val tabLayout:TabLayout = findViewById(R.id.tab_layout)
+        tabLayout.setupWithViewPager(viewPager)
+
+
+
+    }
+
+    private fun setupViewPager(viewPager:ViewPager) {
+        val adapter = ViewPagerAdapter(supportFragmentManager)
+        adapter.addFragment(SimpleFragment(), "Последние")
+        adapter.addFragment(SimpleFragment(), "Избранное")
+        viewPager.adapter = adapter
+
     }
 }
---
+++ RecycleView_4/app/src/main/java/com/example/holmi_production/recycleview_4/SimpleFragment.kt
@@ -0,0 +1,22 @@
+package com.example.holmi_production.recycleview_4
+
+import android.support.v4.app.Fragment
+import android.os.Bundle
+import android.view.ViewGroup
+import android.view.LayoutInflater
+import android.view.View
+
+
+class SimpleFragment: Fragment(){
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.simple_fragment, container, false)
+    }
+}

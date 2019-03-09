4700aa5619fbd91904a7fe5041670df2a53ad7be
AlekseyKholmanov <a.holmanov@gmail.com>
2019-03-10 02:45:55 +0500

    title = theme name

--- RecycleView_4/app/src/main/java/com/example/holmi_production/recycleview_4/ActivityItem.kt
+++ RecycleView_4/app/src/main/java/com/example/holmi_production/recycleview_4/ActivityItem.kt
@@ -8,47 +8,46 @@ import android.view.MenuItem
 import android.widget.TextView
 import android.widget.Toast
 
-class ActivityItem: AppCompatActivity() {
-    private var isFavorite:Boolean? =null
+class ActivityItem : AppCompatActivity() {
+    private var isFavorite: Boolean? = null
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_news_item)
 
-        val themeText  = intent.getStringExtra("theme")
+        val themeText = intent.getStringExtra("theme")
         val contentText = intent.getStringExtra("content")
         val dateText = intent.getStringExtra("date")
-        isFavorite = intent.getBooleanExtra("isFavorite",false)
-        val theme = findViewById<TextView>(R.id.activity_theme)
+        isFavorite = intent.getBooleanExtra("isFavorite", false)
+
         val content = findViewById<TextView>(R.id.activity_content)
         val date = findViewById<TextView>(R.id.activity_date)
 
-        theme.text = themeText
+        title = themeText
         content.text = contentText
-        date.text=dateText
+        date.text = dateText
     }
 
 
     override fun onPrepareOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.activity_item_menu,menu)
-        var drawble = if(isFavorite!!) R.drawable.favorite_enable else R.drawable.favorite_none
+        menuInflater.inflate(R.menu.activity_item_menu, menu)
+        var drawble = if (isFavorite!!) R.drawable.favorite_enable else R.drawable.favorite_none
         menu!!.getItem(0).icon = ContextCompat.getDrawable(this, drawble)
         return true
     }
 
     override fun onOptionsItemSelected(item: MenuItem?): Boolean {
-        if(item!!.itemId == R.id.menu_addFavorite){
-        if(isFavorite!!){
-            item.icon = ContextCompat.getDrawable(this, R.drawable.favorite_none)
-            isFavorite = false
-            Toast.makeText(this,"убрано", Toast.LENGTH_SHORT).show()
-        }
-        else{
-            item.icon = ContextCompat.getDrawable(this, R.drawable.favorite_enable)
-            isFavorite = true
-            Toast.makeText(this,"добавлено", Toast.LENGTH_SHORT).show()
-        }
+        if (item!!.itemId == R.id.menu_addFavorite) {
+            if (isFavorite!!) {
+                item.icon = ContextCompat.getDrawable(this, R.drawable.favorite_none)
+                isFavorite = false
+                Toast.makeText(this, "убрано", Toast.LENGTH_SHORT).show()
+            } else {
+                item.icon = ContextCompat.getDrawable(this, R.drawable.favorite_enable)
+                isFavorite = true
+                Toast.makeText(this, "добавлено", Toast.LENGTH_SHORT).show()
+            }
         }
         return true
     }

557902b7ec7b45f0b949f7595d6b3990dca71f37
a.holmanov <a.holmanov@gmail.com>
2019-03-21 11:23:38 +0500

    delete interface

--- RecycleView_4/app/src/main/java/com/example/holmi_production/recycleview_4/Repository/Repository.kt
+++
@@ -1,8 +0,0 @@
-package com.example.holmi_production.recycleview_4.Repository
-
-public interface Repository<T>{
-    fun add (item:T)
-    fun update(item:T)
-    fun remove(item:T)
-    fun query(specification : Specification)
-}
